cf67e7a77b86c35181964772c62e4c33c8f94127
kchan725 <kcchan725@gmail.com>
2025-02-16 14:21:13 -0500

    initial commit

---
+++ littlelemon/littlelemon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  littlelemon
+//
+//  Created by Kenny Chan on 2/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
